ca89ac27c3e552df850eda56c846b470c61aa8a7
kamadeva <ragecoderid@gmail.com>
2024-12-05 07:59:49 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,190 @@
-import Image from "next/image";
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { FaReact, FaPhp, FaJs } from "react-icons/fa";
+import { FaFlutter, FaDartLang } from "react-icons/fa6";
+import { RiNextjsLine } from "react-icons/ri";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function Home() {
+  const { setTheme } = useTheme();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <nav>
+        <div className="flex h-14 p-4 justify-between items-center backdrop-blur">
+          <div></div>
+          <div className="space-x-4 flex items-center">
+            <Link href={"#home"}>Home</Link>
+            <Link href={"#about"}>About</Link>
+            <Link href={"#contact"}>Contact</Link>
+          </div>
+          <div className="flex items-center">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </nav>
+      <div className="p-8 text-center" id="home">
+        <h1 className="font-bold text-4xl">Software Development</h1>
+        <h2 className="font-bold text-3xl">Fullstack Web Development</h2>
+        <h2 className="font-bold text-3xl">Mobile App Development</h2>
+      </div>
+      <div className="p-8 flex justify-center" id="about">
+        <Tabs defaultValue="overall" className="w-[400px]">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="overall">Overall</TabsTrigger>
+            <TabsTrigger value="web">Web</TabsTrigger>
+            <TabsTrigger value="mobile">Mobile</TabsTrigger>
+          </TabsList>
+          <TabsContent value="overall">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-2xl">Technology overall</CardTitle>
+                <CardDescription>
+                  {`Tech stack usually use for development.`}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  Web Development
+                  <div className="flex space-x-4">
+                    <div>
+                      <FaReact className="text-4xl" />
+                    </div>
+                    <div>
+                      <RiNextjsLine className="text-4xl" />
+                    </div>
+                    <div>
+                      <FaJs className="text-4xl" />
+                    </div>
+                    <div>
+                      <FaPhp className="text-4xl" />
+                    </div>
+                  </div>
+                </div>
+              </CardContent>
+              <CardFooter>
+                <div>
+                  Mobile App Development
+                  <div className="flex space-x-4">
+                    <div>
+                      <FaFlutter className="text-4xl" />
+                    </div>
+                    <div>
+                      <FaDartLang className="text-4xl" />
+                    </div>
+                  </div>
+                </div>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="web">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-2xl">Web dev techstack</CardTitle>
+                <CardDescription>
+                  {`Tech stack usually use for web development.`}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  Frontend
+                  <div className="flex space-x-4">
+                    <div>
+                      <FaReact className="text-4xl" />
+                    </div>
+                    <div>
+                      <RiNextjsLine className="text-4xl" />
+                    </div>
+                    <div>
+                      <FaJs className="text-4xl" />
+                    </div>
+                  </div>
+                </div>
+              </CardContent>
+              <CardFooter>
+                <div>
+                  Backend
+                  <div className="flex space-x-4">
+                    <div>
+                      <FaPhp className="text-4xl" />
+                    </div>
+                  </div>
+                </div>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="mobile">
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-2xl">
+                  Mobile app dev techstack
+                </CardTitle>
+                <CardDescription>
+                  {`Tech stack usually use for mobile app development.`}
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">
+                <div>
+                  Frontend
+                  <div className="flex space-x-4">
+                    <div>
+                      <FaFlutter className="text-4xl" />
+                    </div>
+                    <div>
+                      <FaDartLang className="text-4xl" />
+                    </div>
+                  </div>
+                </div>
+              </CardContent>
+              <CardFooter>
+                <div>
+                  Backend
+                  <div className="flex space-x-4">
+                    <div>
+                      <FaPhp className="text-4xl" />
+                    </div>
+                  </div>
+                </div>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 }
